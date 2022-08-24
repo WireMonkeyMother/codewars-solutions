30b6676d9ba71025e71176a18cd14aca306cc537
CharlieFinch <charles.i.stack@gmail.com>
2022-08-24 10:36:57 -0700

    Codewars solution, done on 8-24-22

---
+++ 8kyu/beginner-lost-without-a-map.js
@@ -0,0 +1,14 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+//MY SOLUTION---
+function maps(x){
+    const y = [];
+    for(let i = 0; i<x.length;i++){
+      y[i] = x[i]*2;
+    }
+    return y;
+  }
