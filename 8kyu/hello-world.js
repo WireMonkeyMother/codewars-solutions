39b67b4cc284c582a3039ec0055620c58e3e0196
CharlieFinch <charles.i.stack@gmail.com>
2022-09-09 14:23:13 -0700

    Codewars Solutions for 9-9-2022

---
+++ 8kyu/hello-world.js
@@ -0,0 +1,22 @@
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative hello world you can think of? What is a "hello world" solution you would want to show your friends?
+
+//MY SOLUTION ---
+
+const greet = () => 'hello world!'
+
+//ADDITIONAL NOTES: For myself, I was most happy to show my friends that I was getting practice with arrow functions, so I went with that. However, in looking through the other responses, I found someone who had made a very clever function that I really enjoyed, and watned to preserve that here as well
+
+// const OPENING = `Call me Ishmael. Some years ago - never mind how long precisely - having little or no money in my purse,
+// and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.
+// It is a way I have of driving off the spleen, and regulating the circulation. Whenever I find myself growing grim about the
+// mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin
+// warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of
+// me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically
+// knocking people's hats off - then, I account it high time to get to sea as soon as I can. This is my substitute for pistol
+// and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the ship.`;
+
+
+// const greet = () => [264, 397, 14, 893, 303, 559, 906, 215, 642, 709, 426].map(n => OPENING[n]).join('') + '!';
