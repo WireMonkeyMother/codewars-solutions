b4a9462e9b9ce678e9d73b2d096d16b825487195
CharlieFinch <charles.i.stack@gmail.com>
2023-01-23 08:39:07 -0800

    Codewars Solutions for 01-23-2023

---
+++ 8kyu/are-you-playing-banjo.js
@@ -0,0 +1,14 @@
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo" 
+// name + " does not play banjo"
+// Names given are always valid strings.
+
+//MY SOLUTION:
+
+const areYouPlayingBanjo = name => name[0].toLowerCase()=='r'? name + ' plays banjo':name + ' does not play banjo' 
+
+//ADDTIONAL NOTES: I've been absent for about 2 months now and this is my first time back. Thankfully, I haven't forgotten everything.
